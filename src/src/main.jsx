fe67d35b1d2d08b6680619f317bc686c31cb3a2e
TotiitoT <totisegundo65@gmail.com>
2026-02-13 21:40:47 -0300

    Update main.jsx

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PriceComparator from './price-comparator.jsx';
+import PriceComparator from '/src/price-comparator.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
